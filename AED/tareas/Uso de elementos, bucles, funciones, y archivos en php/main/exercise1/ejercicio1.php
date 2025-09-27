e5fdfef5b5ce7336b69afa11866f63de60209662
PRORIX <romengilberto08@gmail.com>
2025-09-27 23:18:03 +0100

    Se realiza el primer ejercicio de la tarea 4

---
+++ AED/tareas/Uso de elementos, bucles, funciones, y archivos en php/main/exercise1/ejercicio1.php
@@ -0,0 +1,65 @@
+<?php
+
+/**
+ * EJERCICIO 1: Operacione scon numeros naturales en php
+ * @author prorix
+ * @version 1.0.0
+ */
+
+realizarOperaciones();
+
+/**
+ * Metodo que realiza las operaciones de un archivo y genera otro archivo resultado
+ * @return void
+ */
+function realizarOperaciones() {
+    $rutaArchivoOperaciones = "ops.csv";
+    $rutaDestino = "resultado.csv";
+
+    $archivoOperaciones = fopen($rutaArchivoOperaciones,"r") or die("Ha ocurrido un error abriendo el archivo.");
+
+    $archivoDestino = fopen($rutaDestino,"w") or die("Ha ocurrido un error al abrir el archivo de destino");
+    fwrite($archivoDestino, "z1,z2,op,resultado\n");
+    fgetcsv($archivoOperaciones);
+
+
+    while(($datos = fgetcsv($archivoOperaciones,1000,",")) !== FALSE) {
+        $numero1 = $datos[0];
+        $numero2 = $datos[1];
+        $operacion = $datos[2];
+
+        $resultado = realizarOperacion($numero1,$numero2,$operacion);
+
+        fwrite($archivoDestino, "$numero1,$numero2,$operacion,$resultado\n");
+        
+}
+fclose($archivoOperaciones);
+fclose($archivoDestino);
+}
+
+/**
+ * Metodo que opera dos numeros con una operacion dada
+ * @param mixed $numero1 primer numero de la operacion
+ * @param mixed $numero2 segundo numero de la operacion
+ * @param mixed $operacion operacion deseada por hacer
+ */
+function realizarOperacion ($numero1, $numero2, $operacion) {
+    switch ($operacion) {
+        case "suma":
+            return $numero1 + $numero2;
+        case "resta":
+            return $numero1 - $numero2;
+        case "producto":
+            return $numero1 * $numero2;
+        case "division":
+            if ($numero2 == 0) {
+                return "ERROR";
+            } else {
+                return $numero1 / $numero2;
+            }
+    }
+}
+
+
+
+?>
